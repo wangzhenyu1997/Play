195f260f85fe8e63d8cb58261fe5c6c784966b1f
wangzhenyu <923680072@qq.com>
2021-05-23 13:28:03 +0800

    test

--- app/src/main/java/com/wang/play/BaseActivity.kt
+++ app/src/main/java/com/wang/play/BaseActivity.kt
@@ -3,7 +3,6 @@ package com.wang.play
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.wang.mylibrary.util.MyApplicationLogUtil
 
 open class BaseActivity : AppCompatActivity() {
 
@@ -13,19 +12,6 @@ open class BaseActivity : AppCompatActivity() {
         //使状态栏字体为深色
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 
-        //test 1
-        MyApplicationLogUtil.d("AAAA", "AAAAAAAAAA")
 
-
-        //test 2
-        MyApplicationLogUtil.d("BBBB", "BBBBBBBB")
-
-        //test 3
-        MyApplicationLogUtil.d("CCCC", "CCCCCCCC")
-
-
-
-        //test 4
-        MyApplicationLogUtil.d("DDDD", "DDDDDDDDD")
     }
 }
